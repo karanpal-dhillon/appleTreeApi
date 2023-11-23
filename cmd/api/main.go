3f770497d645c6427e20073e6d3f004240ca28de
Karan Dhillon <Karanpal.dhillon@gmail.com>
2023-11-23 14:02:47 +0530

    initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("A journey of thousand miles begins with a single step")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module appletree.softrelic.co
+
+go 1.21.4
